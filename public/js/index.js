bb060f09eb3ce755515e5f10d1039c962a8b2812
Ron <ron@rapitec.co.il>
2020-05-25 16:04:17 +0300

    filter users list

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const users = [];
-
-const defaultUsers = [
+let numUsers = 0;
+const users = [
     { username: 'Ronro1', email_address: 'ron@rapitec.co.il', full_name: 'Ron Rofe' },
     { username: 'Ronro2', email_address: 'ron@rapitec.co.il', full_name: 'Ron Rofe' },
     { username: 'Ronro3', email_address: 'ron@rapitec.co.il', full_name: 'Ron Rofe' },
@@ -10,6 +9,9 @@ const defaultUsers = [
 ];
 
 // Elements
+const $tableData = document.querySelector('#tableData');
+const $search = document.querySelector('#search');
+
 const $usernameInput = document.querySelector('input[name="username"]');
 const $emailInput = document.querySelector('input[name="email_address"]');
 const $fullNameInput = document.querySelector('input[name="full_name"]');
@@ -21,7 +23,7 @@ const regex = {
     email_address: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
 
-const addUser = (user) => {
+const addUser = (user, id) => {
     const $row = document.createElement('tr');
 
     Object.keys(user).forEach(key => {
@@ -36,7 +38,7 @@ const addUser = (user) => {
     $deleteImg.src = './images/trash.svg';
     $deleteImg.alt = 'Delete';
     $deleteImg.classList.add('action');
-    $deleteImg.setAttribute(`data-delete`, users.length);
+    $deleteImg.setAttribute(`data-delete-id`, id);
 
     const $deleteCell =  document.createElement('td');
     $deleteCell.appendChild($deleteImg);
@@ -48,7 +50,7 @@ const addUser = (user) => {
     $editImg.src = './images/pencil.svg';
     $editImg.alt = 'Edit';
     $editImg.classList.add('action');
-    $editImg.setAttribute(`data-edit`, users.length);
+    $editImg.setAttribute(`data-edit-id`, id);
 
     const $editCell =  document.createElement('td');
     $editCell.appendChild($editImg);
@@ -57,32 +59,33 @@ const addUser = (user) => {
 
     // Delete listener
     $deleteImg.addEventListener('click', function () {
-        const index = +this.getAttribute('data-delete');
+        const index = +this.getAttribute('data-delete-id');
         
         const toDelete = confirm('Are you sure you want to delete this user?');
         
         if (toDelete) {
             users.splice(index ,1);
-            document.querySelectorAll('tr')[index+1].remove();
+            numUsers--;
+            
+            this.parentNode.parentNode.remove(); // td element
 
-            const $new_delete_buttons = document.querySelectorAll('img[data-delete]');
-            for(let i = index; i < $new_delete_buttons.length; i++) { // Update buttons indexes from the place deleted and above
-                $new_delete_buttons[i].setAttribute('data-delete', i);
-                $new_delete_buttons[i].setAttribute('data-edit', i);
+            const $delete_buttons = document.querySelectorAll('img[data-delete-id]');
+            const $edit_buttons = document.querySelectorAll('img[data-edit-id]');
+            for (let i = index; i < $delete_buttons.length; i++) { // Update buttons indexes from the place deleted and above
+                const currentId = +$delete_buttons[i].getAttribute('data-delete-id');
+                $delete_buttons[i].setAttribute('data-delete-id', currentId - 1); // Delete button id
+                $edit_buttons[i].setAttribute('data-edit-id', currentId - 1); // Edit button id
             }
         }
     });
 
     // Edit listener
     $editImg.addEventListener('click', function () {
-        console.log(this.getAttribute('data-edit'));
+        console.log(this.getAttribute('data-edit-id'));
     })
 
     // Add element to the DOM
-    document.querySelector('#tableData').appendChild($row);
-
-    // Add user to array of users
-    users.push(user);
+    $tableData.appendChild($row);
 };
 
 const showError = (id) => {
@@ -93,9 +96,19 @@ const showError = (id) => {
     }, 2500);
 }
 
+const filterUsers = (value) => {
+    $tableData.innerHTML = '';
+
+    users.forEach((user, index) => {
+        if(user.username.toLowerCase().startsWith(value.toLowerCase())) {
+            addUser(user, index);
+        }
+    });
+};
+
 // Insert default users
-for (let user of defaultUsers) {
-    addUser(user);
+for (let user of users) {
+    addUser(user, numUsers++);
 }
 
 // Add user
@@ -127,10 +140,16 @@ document.querySelector('#addButton').addEventListener('click', () => {
     }
 
     // Add values to users array
-    addUser({ username, email_address, full_name });
+    addUser({ username, email_address, full_name }, numUsers);
+    users.push({ username, email_address, full_name });
     
     // Reset form
     $usernameInput.value = '';
     $emailInput.value = '';
     $fullNameInput.value = '';
+
+    filterUsers($search.value);
 });
+
+// Search user
+$search.addEventListener('input', event => filterUsers(event.target.value));
